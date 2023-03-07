bbf2beff25d3ac9c5820f2a1fe3e5225d1c09c3d
Berkay Özgün <berkayozgun001@gmail.com>
2023-03-07 12:51:46 +0300

    body created and header texts added

--- beije_test/Body.js
+++ beije_test/Body.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
-import {View, Text} from 'react-native';
-import Footer from './Footer';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  bodyContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    height: 600,
+  },
+  bodyHeader: {
+    fontSize: 32,
+    fontWeight: 'semibold',
+    color: '#000',
+    textAlign: 'left',
+    marginTop: 100,
+  },
+  bodyText: {
+    fontSize: 20,
+    color: 'grey',
+    textAlign: 'left',
+    width: 350,
+    lineHeight: 30,
+    marginTop: 20,
+  },
+});
 
 export default function componentName() {
   return (
-    <View>
-      <Text>Kendi paketini oluştur.</Text>
+    <View style={styles.bodyContainer}>
+      <Text style={styles.bodyHeader}>Kendi paketini oluştur.</Text>
+
+      <Text style={styles.bodyText}>
+        Tercih ve ihtiyaçların doğrultusunda seçeceğin ürünlerden ve
+        miktarlardan, sana özel bir paket oluşturalım.
+      </Text>
     </View>
   );
 }
